47abdd303714b2300682004f89699300bc017ff3
Noémie Rigault <noo.riagult@gmail.com>
2025-04-29 16:55:11 +0200

    Ajout de la modale et du bandeau noir si connecté et possibilité de supprimer un projet

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -1,7 +1,18 @@
-async function init() {
+async function init() {    
     await displayGallery()
     await displayCategories()
-}
+    
+    const loginLink = document.getElementById("login-link");
+    if (localStorage.getItem("token")) {
+        loginLink.textContent = "logout";
+        loginLink.href = "#";
+        loginLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            localStorage.removeItem("token");
+            window.location.href = "index.html";
+    });
+
+}}
 
 let allProjects = []; // Variable globale accessible partout dans toutes les fonctions
 
@@ -34,7 +45,6 @@ async function displayCategories() {
     const allCategories = await reponse.json()
 
     const categories = document.querySelector(".filters-container");
-    //Il faudra ajouter ici une variable avec tous les projets dedans
 
     //Création du bouton "Tous"
     const buttonAll = document.createElement("button");
@@ -63,8 +73,67 @@ async function displayCategories() {
 
         categories.appendChild(buttonElement);
     });
+
     
 }
+
+// Récupère tous les liens du menu
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
+});
+
+//Permet d'ajouter le bloc "modifier" à la connexion uniquement
+document.addEventListener("DOMContentLoaded", () => {
+	const token = localStorage.getItem("token");
+	const openModal = document.querySelector(".openModal");
+
+	if (token && openModal) {
+		openModal.style.display = "flex";
+	}
+
+//Permet de supprimer les boutons de filtre en mode déconnexion uniquement
+    const filtersContainer = document.querySelector(".filters-container");
+	if (token && filtersContainer) {
+		filtersContainer.style.display = "none";
+	}
+});
+
+
+//---------------------------------Création du bandeau noir si connecté---------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+    const token = localStorage.getItem("token");
+  
+    if (token) {
+      // Affiche la bannière édition sans toucher au reste
+      const body = document.getElementById('body');
+  
+      const divBanner = document.createElement('div');
+      divBanner.id = 'divBanner';
+      divBanner.className = 'banner';
+  
+      const iconBanner = document.createElement('i');
+      iconBanner.className = 'fa-regular fa-pen-to-square';
+  
+      const editionBanner = document.createElement('p');
+      editionBanner.textContent = 'Mode édition';
+  
+      divBanner.appendChild(iconBanner);
+      divBanner.appendChild(editionBanner);
+  
+      body.insertBefore(divBanner, body.firstChild);
+
+      body.style.marginTop = '40px';
+    }
+  });
+    
+
+  
+
 
 //Pour dire d'attendre que le contenu du DOM soit bien chargé avant d'éxécuter le fichier JS
 document.addEventListener('DOMContentLoaded', init);
