2d361ec0be057f7512412328fba997840b3406fb
Noémie Rigault <noo.riagult@gmail.com>
2025-05-04 15:18:48 +0200

    Ajouter un projet et début de mise au propre

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -1,21 +1,14 @@
+// Variable globale accessible partout dans toutes les fonctions
+let allProjects = []; 
+let allCategories = [];
+
 async function init() {    
     await displayGallery()
     await displayCategories()
-    
-    const loginLink = document.getElementById("login-link");
-    if (localStorage.getItem("token")) {
-        loginLink.textContent = "logout";
-        loginLink.href = "#";
-        loginLink.addEventListener("click", (event) => {
-            event.preventDefault();
-            localStorage.removeItem("token");
-            window.location.href = "index.html";
-    });
+    displayAdminDashboard()
+}
 
-}}
-
-let allProjects = []; // Variable globale accessible partout dans toutes les fonctions
-
+//Récupération et affichage des projets dans la gallerie
 async function displayGallery(projects) {
     if (!projects){
         const reponse = await fetch("http://localhost:5678/api/works")
@@ -42,19 +35,17 @@ async function displayGallery(projects) {
 
 async function displayCategories() {
     const reponse = await fetch("http://localhost:5678/api/categories")
-    const allCategories = await reponse.json()
+    allCategories = await reponse.json()
 
     const categories = document.querySelector(".filters-container");
-
-    //Création du bouton "Tous"
     const buttonAll = document.createElement("button");
+
     buttonAll.innerText = "Tous"
     buttonAll.classList.add("filter-button");
 
     buttonAll.addEventListener("click", function () {
         displayGallery();
     });
-
 
     categories.appendChild(buttonAll);
     
@@ -77,64 +68,64 @@ async function displayCategories() {
     
 }
 
+function displayAdminDashboard() {
+    const token = localStorage.getItem("token");
+	const openModal = document.querySelector(".openModal");
+    const filtersContainer = document.querySelector(".filters-container");
+    const loginLink = document.getElementById("login-link");
+
+
+    // Affiche le bouton "modifier"
+    // Cache les boutons filtres
+    // Passe "Login" en "Logout" dans le menu
+    // Affiche le bandeau noir
+    if (token) {
+
+        if (openModal) {
+            openModal.style.display = "flex";
+        }
+
+        if (filtersContainer) {
+            filtersContainer.style.display = "none";
+        }
+
+        loginLink.textContent = "logout";
+        loginLink.href = "#";
+        loginLink.addEventListener("click", (event) => {
+            event.preventDefault();
+            localStorage.removeItem("token");
+            window.location.href = "index.html";
+        });
+
+        const body = document.getElementById('body');
+    
+        const divBanner = document.createElement('div');
+        divBanner.className = 'banner';
+    
+        const iconBanner = document.createElement('i');
+        iconBanner.className = 'fa-regular fa-pen-to-square';
+    
+        const editionBanner = document.createElement('p');
+        editionBanner.textContent = 'Mode édition';
+    
+        divBanner.appendChild(iconBanner);
+        divBanner.appendChild(editionBanner);
+    
+        body.insertBefore(divBanner, body.firstChild);
+  
+        body.style.marginTop = '40px';
+    }
+}
+
+
 // Récupère tous les liens du menu
 const navLinks = document.querySelectorAll("nav a");
-
+// Met en surbrillance le lien actif
 navLinks.forEach(link => {
   if (link.href === window.location.href) {
     link.classList.add("active");
   }
 });
 
-//Permet d'ajouter le bloc "modifier" à la connexion uniquement
-document.addEventListener("DOMContentLoaded", () => {
-	const token = localStorage.getItem("token");
-	const openModal = document.querySelector(".openModal");
-
-	if (token && openModal) {
-		openModal.style.display = "flex";
-	}
-
-//Permet de supprimer les boutons de filtre en mode déconnexion uniquement
-    const filtersContainer = document.querySelector(".filters-container");
-	if (token && filtersContainer) {
-		filtersContainer.style.display = "none";
-	}
-});
-
-
-//---------------------------------Création du bandeau noir si connecté---------------------------------
-
-document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
-  
-    if (token) {
-      // Affiche la bannière édition sans toucher au reste
-      const body = document.getElementById('body');
-  
-      const divBanner = document.createElement('div');
-      divBanner.id = 'divBanner';
-      divBanner.className = 'banner';
-  
-      const iconBanner = document.createElement('i');
-      iconBanner.className = 'fa-regular fa-pen-to-square';
-  
-      const editionBanner = document.createElement('p');
-      editionBanner.textContent = 'Mode édition';
-  
-      divBanner.appendChild(iconBanner);
-      divBanner.appendChild(editionBanner);
-  
-      body.insertBefore(divBanner, body.firstChild);
-
-      body.style.marginTop = '40px';
-      console.log("ici")
-    }
-  });
-    
-
-  
-
-
-//Pour dire d'attendre que le contenu du DOM soit bien chargé avant d'éxécuter le fichier JS
+//Pour dire d'attendre que le contenu du DOM soit bien chargé avant d'éxécuter la fonction init
 document.addEventListener('DOMContentLoaded', init);
